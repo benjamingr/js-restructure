13c63c2152cf2d49e6664b2dc9622b9927a4c60a
Benjamin Gruenbaum <inglor@gmail.com>
2016-01-25 17:28:35 +0200

    tests

--- tests.js
+++ tests.js
@@ -5,4 +5,32 @@ describe("Matching", function() {
     var m = matcher({x:"[A-Z]"})("A");
     assert.equal(m.x, "A");
   });
+  it("matches two string", function(){
+    var m = matcher({x:"[A-Z]", y:"[A-Z]"})("AB");
+    assert.equal(m.x, "A");
+    assert.equal(m.y, "B");
+  });
+  it("matches multi character strings", function(){
+    var m = matcher({x:"[A-Z]+"})("AB");
+    assert.equal(m.x, "AB");
+  });
+  it("matches lazily when it has to", function(){
+    var m = matcher({x:"[A-Z]+?", _:"C"})("ABCD");
+    assert.equal(m.x, "AB");
+  });
+  it("matches numbers", function(){
+    var m = matcher({x:"\\d+"})("123123");
+    assert.equal(m.x, "123123");
+  });
+  it("matches emails", function(){
+    var m = matcher({
+	   _    : "^",
+	   user : "\\w+",
+	   _2   : "@",
+	   host : "[^@]+",
+	   _3    : "$"
+	})("benji@nono.com")
+    assert.equal(m.user, "benji");
+    assert.equal(m.host, "nono.com");
+  });
 });
