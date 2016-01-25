55746b5cf588b6f29f7f1440cd429e08f94eab4a
Benjamin Gruenbaum <inglor@gmail.com>
2016-01-25 17:08:14 +0200

    es2015 -> es5

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function matcher(obj) { "use strict";
-   let props = Object.getOwnPropertyNames(obj);
-   const re = new RegExp(props.reduce((p, c) => p + (c.startsWith("_") ? obj[c] : `(${obj[c]})`), ""));
-   props = props.filter(x => !x.startsWith("_"));
+module.exports = function matcher(obj) {
+   "use strict";
+   var props = Object.getOwnPropertyNames(obj);
+   var re = new RegExp(props.reduce(function(p, c) {
+      return p + (c.startsWith("_") ? obj[c] : "("+obj[c]}+")";
+   }));
+   props = props.filter(function(x) { return !x.startsWith("_"); });
    return function(pattern) { 
-      let o = {};
-      const res = re.exec(pattern);
-      for(let i = 0; i < res.length; i++) o[props[i]] = res[i+1];
+      var o = {};
+      var res = re.exec(pattern);
+      for(var i = 0; i < res.length; i++) {
+         o[props[i]] = res[i + 1];
+      }
       return o;
    };
 }
